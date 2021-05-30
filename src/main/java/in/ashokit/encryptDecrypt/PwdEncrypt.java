d961450112e6e1a91ae916b48a47203e10591f5f
Kumari Sinam <sinamkri55@gmail.com>
2021-05-30 19:56:39 +0200

    changed 1

--- src/main/java/in/ashokit/encryptDecrypt/PwdEncrypt.java
+++ src/main/java/in/ashokit/encryptDecrypt/PwdEncrypt.java
@@ -6,7 +6,7 @@ import java.util.Base64.Encoder;
 public class PwdEncrypt {
 	
 	public static String encode(String pwd) {
-		int a=10;
+		//int a=10;
 		Encoder encoder = Base64.getEncoder();
 		String encodedString = encoder.encodeToString(pwd.getBytes());
 		
